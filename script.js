60d47ff1e600de2440b43c066a09cec713d417e9
Poonam <poonam.trifed@gmail.com>
2023-05-02 22:36:09 +0530

    mobile display add and final touch done

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+
+let i, tabcontent, tablinks;
+tabcontent = document.getElementsByClassName("tabcontent");
+tablinks = document.getElementsByClassName("tablinks");
 function openPage(evt, pageName){
-    let i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
+    
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
       }
-      tablinks = document.getElementsByClassName("tablinks");
+     
       for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
       }
       document.getElementById(pageName).style.display = "block";
       evt.currentTarget.className += " active";
 }
+
+window.onload = function() {
+    tabcontent[0].style.display = "block";
+  };
